95931ff25c5083a2c1d05b030b97184976899816
leonelcifuentes <leocifuentes100907@gmail.com>
2025-10-07 16:50:09 -0300

    repaso de media querys;creacion de primera interfax y uso de directiva'ngFor'para tarjeta

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,13 @@
+/*ng g i nolmbreInterfas*/
+/*ng generate interface nombreinterfaz
+
+*/
+export interface Tarjeta {
+    id:string; //numeros como letras-> ej.:123ab/ 123 / abc
+    img:string,
+    alt:string,
+    titulo: string;
+    descripcion:string;
+    anualpublicacion:number;
+
+}
--- src/app/pages/product/product.ts
+++ src/app/pages/product/product.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tarjeta } from '../../models/tarjeta';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css'
 })
 export class Product {
+  /**
+   * propiedad PUBLICA: libere acceso a su contenido / 
+   * PRIVADA: contenido restringido
+   */
+  public infoTarjeta: Tarjeta[];
 
+  /*declaramos coleccion de tarjetas en base a modelo de interfaz "tarjeta
+  -se tiene que respetar el usode todas las propiedades y su tipo
+  -tipo alfanumérico (string) inicializa en ""
+  -tipo numérico(number) inicializa en 0
+  Q 1|°   ° */
+  constructor() {
+    this.infoTarjeta = [
+      {
+        id:"1T",
+        img:"../../../assets/puerta5product.jpg",
+        alt:"puertaaa",
+        titulo:"puertas",
+        descripcion:"puertas para casa",
+        anualpublicacion:2025
+      },
+        {
+        id:"2T",
+        img:"../../../assets/puerta6product.jpg",
+        alt:"puertaaa",
+        titulo:"puertas",
+        descripcion:"puertas para casa",
+        anualpublicacion:2025
+      },
+        {
+        id:"3T",
+        img:"../../../assets/puerta7product.jpg",
+        alt:"puertaaa",
+        titulo:"puertas",
+        descripcion:"puertas para casa",
+        anualpublicacion:2025
+      },
+        {
+        id:"4sT",
+        img:"../../../assets/puerta5product.jpg",
+        alt:"puertaaa",
+        titulo:"puertas",
+        descripcion:"puertas para casa",
+        anualpublicacion:2025
+      }
+
+    ]
+  }
 }
