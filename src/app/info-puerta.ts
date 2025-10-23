9df99e4e22956b78e592b0574334edcbca74c2fc
LeonelCifuentess <leocifuentes100907@gmail.com>
2025-10-23 16:57:36 -0300

    continuamos con el nuevointerfaz

---
+++ src/app/info-puerta.ts
@@ -0,0 +1,8 @@
+export interface InfoPuerta {
+    id:string;
+    img:string;
+    titulo:string;
+    color:string;
+    material:string;
+    anualCreacion:number;
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,53 +1,60 @@
 import { Component } from '@angular/core';
 import { Puerta } from '../../puerta';
 import { identifierName } from '@angular/compiler';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-public infoPuerta:Puerta[];
+  public infoPuerta: Puerta[];
 
-constructor(){
-  this.infoPuerta=[
-    {
-      id:"P1",
-      img:"",
-      titulo:"",
-      color:"",
-      material:"",
-      anualCreacion:1,
+  constructor() {
+    this.infoPuerta = [
+      {
+        id: "P1",
+        img: "../../../assets/product8home",
+        titulo: "puerta para casa",
+        color: "Negra y Dorado",
+        material: "Aluminio",
+        anualCreacion: 2010,
 
-  },
-    {
-  id:"P2",
-      img:"",
-      titulo:"",
-      color:"",
-      material:"",
-      anualCreacion:2,
-},
-    {
-  id:"P3",
-      img:"",
-      titulo:"",
-      color:"",
-      material:"",
-      anualCreacion:2,
-     } ,
-    {  
-  id:"P4",
-      img:"",
-      titulo:"",
-      color:"",
-      material:"",
-      anualCreacion:2,
-    },
+      },
+      {
+        id: "P2",
+        img: "../../../assets/product9home",
+        titulo: "puerta para bodega",
+        color: "Marrón",
+        material: "Madera",
+        anualCreacion: 1900,
+      },
+      {
+        id: "P3",
+        img: "../../../assets/product10home",
+        titulo: "puerta para casa",
+        color: "Blanca",
+        material: "Madera",
+        anualCreacion: 2000,
+      },
+      {
+        id: "P4",
+        img: "../../../assets/puertas11home",
+        titulo: "puerta para iglesia",
+        color: "Marrón",
+        material: "Madera",
+        anualCreacion: 1990,
+      },
 
 
-  ]
-}
+    ]
+  }
+  tarjetaSeleccionada: any = null;
+
+  /*recibir elemento seleccionado por parametro y lo asignaa "tarjetaSeleccionada" */
+  verMas(tarjeta: any) {
+    this.tarjetaSeleccionada = tarjeta;
+  }
 }
